1cbdcf1f9d12371573b889fe12a30312419cd965
Rinshin-Jalal <rinzhinjalal@gmail.com>
2025-10-01 16:46:16 +0530

    Initial Commit

---
+++ bigbruhh/bigbruhhApp.swift
@@ -0,0 +1,17 @@
+//
+//  bigbruhhApp.swift
+//  bigbruhh
+//
+//  Created by Rinshin on 01/10/25.
+//
+
+import SwiftUI
+
+@main
+struct bigbruhhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
